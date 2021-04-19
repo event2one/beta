ce0b8a66c7a07904a46af1d319697ec4e713ed0d
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-19 18:09:18 +0200

    Correction de bug premier composant

--- docs/components/Articles/index.js
+++ docs/components/Articles/index.js
@@ -10,35 +10,35 @@ class PressRelease extends HTMLElement {
             <div class="modal">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                        <div class="modal-body" id="eventList">dsqqd</div>
+                        <div class="modal-body" id="articleList"></div>
                     </div>
                 </div>
             </div>
         </section>`;
 
-        this.fetchEvents();
+        this.fetchArticles();
     }
 
 
-    displayEvents = ({ events }) => {
-        events.map((event, index) => this.Event({ event, index })).join('');
-        document.getElementById("eventList").innerHTML = this.content;
+    displayArticles = ({ articles }) => {
+        articles.map((article, index) => this.Article({ article, index })).join('');
+        document.getElementById("articleList").innerHTML = this.content;
     }
 
-    Event = ({ events }) => {
+    Article = ({ article }) => {
         const content = `
-            <div><a href="http://www.mlg-consulting.com/Events/EventPrestas${events.id_event_presta}">${event_presta_nom}</a></div>
+            <div><a href="http://www.mlg-consulting.com/Events/EventPrestas${article.id_event_presta}">${article.event_presta_nom}</a></div>
         `
 
         return content;
     }
  
-    fetchEvents = async () => {
+    fetchArticles = async () => {
         
         await fetch(`https://www.mlg-consulting.com/smart_territory/form/api.php?action=getEventPrestas&id_event=1635`)
                 .then(res = res.json())
-                .then(eventList => {
-                    this.displayEvents({ events: eventList });
+                .then(articleList => {
+                    this.displayArticles({ articles : articleList });
                 })
     }
 }
