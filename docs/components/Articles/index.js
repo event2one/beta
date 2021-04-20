5f14090cba2b094cead654c6ea87c45a6dd9c3d5
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-20 10:36:19 +0200

    test dark link

--- docs/components/Articles/index.js
+++ docs/components/Articles/index.js
@@ -9,9 +9,12 @@ class PressRelease extends HTMLElement {
             <div class="container">
                     <h2>Press releases</h2>
                     <div class="row" id="articleList"></div>
-                <div class="row">
-                    <a class="col align-self-end" href="https://lienverstouslesarticles.html">Voir tous les articles</a>
-                </div>       
+                <div class="container">
+                    <div class="row">
+                        <a class="col align-self-end link-dark" href="https://lienverstouslesarticles.html">Voir tous les articles</a>
+                    </div>
+                </div>
+                           
             </div>
                 
         </section>`;
