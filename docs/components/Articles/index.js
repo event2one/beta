7e3818fd2a7a82644685f65c57fcbd1c08047a7a
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-20 10:52:36 +0200

    update bootstrap link

--- docs/components/Articles/index.js
+++ docs/components/Articles/index.js
@@ -3,16 +3,17 @@ class PressRelease extends HTMLElement {
     constructor() {
         
         super();
-    //     <div class="container">
-    //     <div class="row">
-    //         <a class="col col-lg-2 align-self-end link-dark" href="https://lienverstouslesarticles.html">Voir tous les articles</a>
-    //     </div>
-    // </div>
+   
         this.innerHTML = `
         <section>
             <div class="container">
                     <h2>Press releases</h2>
-                    <div class="row row-cols-3" id="articleList"></div>
+                    <div class="row" id="articleList"></div>
+                    <div class="container">
+                        <div class="row">
+                            <a class="col-2 align-self-end link-dark" href="https://lienverstouslesarticles.html">Voir tous les articles</a>
+                        </div>
+                    </div>
             </div>
                 
         </section>`;
@@ -30,10 +31,7 @@ class PressRelease extends HTMLElement {
         if(index < 3) {
             const content = `<div class="col"><a class="link-dark" href="http://www.mlg-consulting.com/Events/EventPrestas${article.id_event_presta}">${article.event_presta_nom}</a></div>`
             return content;
-        } else if(index == 4){
-            const content = `<div class="col align-self-end"><a class="link-dark" href="https://lienverstouslesarticles.html">Voir tous les articles</a></div>`
-            return content;
-        }
+        } 
     }
  
     fetchArticles = async () => {
