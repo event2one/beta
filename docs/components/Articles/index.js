4e540a2aca4476b82858126d6161bf1d54b22f3a
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-20 10:43:43 +0200

    test alignement à droite lien

--- docs/components/Articles/index.js
+++ docs/components/Articles/index.js
@@ -3,18 +3,16 @@ class PressRelease extends HTMLElement {
     constructor() {
         
         super();
-
+    //     <div class="container">
+    //     <div class="row">
+    //         <a class="col col-lg-2 align-self-end link-dark" href="https://lienverstouslesarticles.html">Voir tous les articles</a>
+    //     </div>
+    // </div>
         this.innerHTML = `
         <section>
             <div class="container">
                     <h2>Press releases</h2>
-                    <div class="row" id="articleList"></div>
-                <div class="container">
-                    <div class="row">
-                        <a class="col align-self-end link-dark" href="https://lienverstouslesarticles.html">Voir tous les articles</a>
-                    </div>
-                </div>
-                           
+                    <div class="row row-cols-3" id="articleList"></div>
             </div>
                 
         </section>`;
@@ -31,8 +29,9 @@ class PressRelease extends HTMLElement {
     Article = ({ article, index }) => {
         if(index < 3) {
             const content = `<div class="col"><a class="link-dark" href="http://www.mlg-consulting.com/Events/EventPrestas${article.id_event_presta}">${article.event_presta_nom}</a></div>`
-
-            // const  res =  `<div>${content}</div>`;
+            return content;
+        } else {
+            const content = `<div class="col align-self-end"><a class="link-dark" href="https://lienverstouslesarticles.html">Voir tous les articles</a></div>`
             return content;
         }
     }
