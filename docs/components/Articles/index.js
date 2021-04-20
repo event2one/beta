124db4dac14ec52b28b39ee2b95bef8fc860dbaf
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-20 11:36:05 +0200

    h1

--- docs/components/Articles/index.js
+++ docs/components/Articles/index.js
@@ -7,7 +7,7 @@ class PressRelease extends HTMLElement {
         this.innerHTML = `
         <section>
             <div class="container">
-                    <h2 class="text-secondary">Press releases</h2>
+                    <h1 class="text-secondary">Press releases</h1>
                     <div class="row" id="articleList">orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
                     <hr/>
                     <a href="https://lienverstouslesarticles.html" style="float: right">Voir tous les articles</a>
