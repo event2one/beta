20096933c522dcb25d03795102d0bf93f02d8769
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-21 16:24:49 +0200

    Remise en place des composants

--- docs/components/ThematicForm/index.js
+++ docs/components/ThematicForm/index.js
@@ -6,15 +6,44 @@ class ThematicForm extends HTMLElement {
         this.innerHTML = `
                         <section>
                             <div class="container">
-                                <h1 class="text-dark">Choix de mes collectifs technologiques</h1>
+                                <h1 class="" style="color: white; background-color: #25555c">Choix de mes collectifs technologiques</h1>
                                 <p>Si ma solution s'inscrit dans uen thématique technologique, je sélectionne la thématique qui lui correspond ci-dessous</p>
-                                <input class="btn btn-primary" type="checkbox" id="checkbox1"></input>
-                                <label for="checkbox1">Collectif des experts - BlueTech</label>
+                                <div class="container" id="thematicCheckbox"></div>
+                                
                             </div> 
                         </section>
                     `
+
+                    this.fetchThematicForm();
     }
-}
+
+            displayThematicForm = ({ thematicCheckboxs }) => {
+                let res = thematicCheckboxs.filter(thematicCheckbox => thematicCheckbox.afficher != 'n').map((thematicCheckbox, index) => this.thematicCheckbox({ thematicCheckbox})).join('');
+                document.getElementById("thematicCheckbox").innerHTML = res;
+            }
+
+            thematicCheckbox = ({ thematicCheckbox }) => {
+
+                    const content = `
+                                <div class="row">
+                                    <input class="btn-primary" type="checkbox" id="checkbox1"></input>
+                                    <label for="checkbox1">${thematicCheckbox.nom}</label>
+                                </div>
+                            `;
+
+                return content;    
+            }
+
+            fetchThematicForm = async () => {
+                
+                const req_suite = `params=where%20id_event!=399%20and`;
+
+                await fetch(`//www.mlg-consulting.com/smart_territory/form/api.php?action=getEvents&${req_suite}`)
+                        .then(res => res.json())
+                        .then(thematicForm => {
+                            this.displayThematicForm({ thematicCheckboxs : thematicForm });
+                        })
+            }
+    }
 
 customElements.define('thematic-form', ThematicForm);
-//                                 <div class="container" id="nextEvent" style="box-shadow: 0px 0px 5px 1px grey"></div>
