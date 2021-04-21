b8de866ef05263bcf61a71e25916012fa3d5fd85
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-21 16:56:35 +0200

    changement html pressrelease pour actus

--- docs/components/PressRelease/index.js
+++ docs/components/PressRelease/index.js
@@ -7,10 +7,9 @@ class PressRelease extends HTMLElement {
         this.innerHTML = `
         <section>
             <div class="container">
-                    <h1 class="text-secondary">Press releases</h1>
+                    <h1 class="text-secondary">Actus</h1>
                     <div class="row row-cols-1 row-cols-md-3" id="articleList"></div>
-                    <hr/>
-                    <a href="https://lienverstouslesarticles.html" style="float: right">Voir tous les articles</a>
+                    <button>Toutes les actus</button>
             </div> 
         </section>`;
 
@@ -25,7 +24,18 @@ class PressRelease extends HTMLElement {
 
     Article = ({ article, index }) => {
         if(index < 3) {
-            const content = `<div class="col"><a class="link-dark" href="http://www.mlg-consulting.com/Events/EventPrestas${article.id_event_presta}">${article.event_presta_nom} Acrius vero ea salutis considerans ad mitigabat vero incertum ad impetu ingenii idque praesens augeri discrimina vexillum potius altius actus.</a></div>`;
+            const content = `
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="card-title">${article.event_presta_nom}</div>
+                                    <img src="" class="card-img"></card>
+                                    <p class="card-text">${article.texte_contextuel}</p>
+                                    <p class="card-text">${}</p>
+                                    <a href="https://lirelasuite" class="card-link text-end">Lire la suite</a>
+                                </div>
+                            </div>
+            <div class="col"><a class="link-dark" href="http://www.mlg-consulting.com/Events/EventPrestas${article.id_event_presta}">${article.event_presta_nom} Acrius vero ea salutis considerans ad mitigabat vero incertum ad impetu ingenii idque praesens augeri discrimina vexillum potius altius actus.</a></div>
+            `;
             return content;
         } 
     }
