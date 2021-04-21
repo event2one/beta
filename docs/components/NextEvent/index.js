a0f6cbd6e73c647eab4221f7a1aac3d9568c386e
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-21 10:18:19 +0200

    correction border et colonne

--- docs/components/NextEvent/index.js
+++ docs/components/NextEvent/index.js
@@ -25,13 +25,13 @@ class NextEvent extends HTMLElement {
     infoEvent = ({ infoEvent, index }) => {
         if(index < 2) {
             const content = `
-                        <div class="row" style="border: 1px solid grey">
+                        <div class="row" style="border-bottom: 1px solid grey">
                             <div class="col-2">
                                 <p>${infoEvent.precision_date}</p>
                                 <p>${infoEvent.nom}</p>
                                 <img src="${infoEvent.logo}"></img>
                             </div>
-                            <div class="col-8">
+                            <div class="col-10">
                                 <p>2 continents, 7 pays, 10 médias, 12 territoires et +3000 décideurs en ligne chaque jour. Le Village Francophone vous offre le dispositif le plus important d'intelligence
                                 collective jamais mis en oeuvre lors du South by SouthWest avec au programme : </br> </br> 2 après-midis marathon pour décrypter 15 tendances clés pour l'Entertainment
                                 de la fashiontech, beautyTech, foodTech</p>
