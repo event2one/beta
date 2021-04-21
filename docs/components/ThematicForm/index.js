b18990fabb4959f2b34f0256cae7265b0545127e
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-21 15:49:54 +0200

    ajout d'un style au bouton

--- docs/components/ThematicForm/index.js
+++ docs/components/ThematicForm/index.js
@@ -8,7 +8,7 @@ class ThematicForm extends HTMLElement {
                             <div class="container">
                                 <h1 class="text-dark">Choix de mes collectifs technologiques</h1>
                                 <p>Si ma solution s'inscrit dans uen thématique technologique, je sélectionne la thématique qui lui correspond ci-dessous</p>
-                                <input type="checkbox" id="checkbox1"></input>
+                                <input class="btn btn-primary" type="checkbox" id="checkbox1"></input>
                                 <label for="checkbox1">Collectif des experts - BlueTech</label>
                             </div> 
                         </section>
