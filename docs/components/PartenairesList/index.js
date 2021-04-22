c4b27e95b0a432870c4c2d0f2bfca6c86d7acc1a
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-22 17:28:58 +0200

    ajout du composant PartenairesList : Carousel + 2 fonctions async

---
+++ docs/components/PartenairesList/index.js
@@ -0,0 +1,64 @@
+class PartenairesList extends HTMLElement {
+
+    constructor() {
+        super();
+
+        this.innerHTML = `<div class="container">
+                            <h2 class="mb-4">Les partenaires et solutions</h2>
+                              <div class="carouselslider">
+                                <input class="carousel-control previous carousel-previous" type="button" value=""></input>
+                                <div class="carousel-wrap">
+                                    <div class="margin-fixer">
+                                        <ul class="item_list" id="partenairesList"></ul>
+                                    </div>
+                                </div>
+                              </div>
+                          </div>`;
+
+        this.fetchContactList();
+    }
+
+    displayInfoContacts = ({ infoContacts }) => {
+
+        let res = infoContacts.map((infoContact) => this.infoContact({ infoContact })).join('');
+        document.getElementById("partenairesList").innerHTML = res;
+
+    }
+
+    infoContact = ({ infoContact }) => {
+
+        const content = `
+                         <li style="height: 200px;">
+                            <a href="infoContact">
+                                <img src="https://www.event2one.com/screen_manager/content/logos/${infoContact.logo}"></img>
+                            </a>
+                         </li>
+                        `;
+
+        return content;
+    }
+
+    fetchContactList = async () => {
+
+        const req = `getContactConferencierList&filter=%20and%20id_event=1635`;
+
+        await fetch(`https://www.mlg-consulting.com/smart_territory/form/api.php?action=${req}`)
+            .then(res => res.json())
+            .then(contactList => {
+                this.fetchInfoContact({ infoContactList : contactList });
+            })
+    }
+
+    fetchInfoContact = async ({ infoContactList }) => {
+
+        const req_id_contact = `${infoContactList.id_contact}`
+
+        await fetch(`https://www.mlg-consulting.com/smart_territory/form/api.php?action=${req_id_contact}`)
+            .then(res => res.json())
+            .then(infoContactList => {
+                this.displayInfoContacts({ infoContacts : infoContactList });
+            })
+    }
+}
+
+customElements.define('partenaires-list', PartenairesList);
