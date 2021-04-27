cb5950f7e0e61298a109f2b1645bf8ee34aa0a94
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-04-27 09:52:42 +0200

    update css composant pressList

--- docs/components/PressList/index.js
+++ docs/components/PressList/index.js
@@ -5,11 +5,42 @@ class PressList extends HTMLElement {
         super();
    
         this.innerHTML = `
+        <style>
+            .pressArticle {
+                padding: 1rem;
+                box-shadow: 0 0 20px rgba(75, 75, 75, 0.1);
+                border-radius: 2rem;
+                display: flex;
+                flex-direction: row;
+            }
+
+            h1 {
+                text-align: center;
+                font-size: 50px;
+                font-weight: black;
+            }
+
+            h4 {
+                font-size: 30px;
+                font-weight: bold;
+            }
+            
+            p:first-child {
+                font-size: 25px;
+                font-weight: light;
+            }
+
+            p {
+                font-size: 30px;
+                font-weight: regular;
+            }
+
+        </style>
         <section>
             <div class="container">
                 <h1 class="text-secondary">Presse</h1>
                 <p>Vous souhaitez vous tenir informé de l'actualité du salon, consulter les communiqués ou dossier de presse, effectuer une demande d’accréditation ou encore entrer en contact avec nous ? Cet espace est fait pour vous.</p>
-                <h1 class="text-secondary">On en parle dans la presse</h1>
+                <h2 class="text-secondary">On en parle dans la presse</h2>
                 <hr/>
                 <div id="pressList"></div>
             </div> 
@@ -26,11 +57,11 @@ class PressList extends HTMLElement {
 
     Article = ({ article }) => {
 
-            const content = `
-                                    <div>
-                                        ${article.media_nom}
-                                        ${article.date_parution}
-                                        ${article.event_presta_nom}
+            const content = `   <div class="pressArticle">
+                                    <div class="infoArticle">
+                                        <h4>${article.media_nom}</h4>
+                                        <p>${article.date_parution}</p>
+                                        <p>${article.event_presta_nom}</p>
                                     </div>
                                     <div>
                                         <img src="${article.flag}" style="width:35%" />
@@ -38,6 +69,8 @@ class PressList extends HTMLElement {
                                     <div>
                                         <a href="${article.event_presta_url_externe}">Découvrir</a>
                                     </div>
+                                </div>
+                                    
                             `;
 
             return content;
