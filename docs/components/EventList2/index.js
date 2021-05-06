a727b910b2fead9843622a6d90259af0f763a875
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-05-06 09:31:16 +0200

    test avec seulement visuel

--- docs/components/EventList2/index.js
+++ docs/components/EventList2/index.js
@@ -35,13 +35,13 @@ class EventList2 extends HTMLElement {
   Event = ({ event, index }) => {
     const isActive = index == 0 ? "active" : "";
 
-    let visuelComplet;
+    // let visuelComplet;
 
     // const visuel = event.lieu.visuel_principal != "" ? `<img src="${event.lieu.visuel_principal}" style="width:100%">` : "";
-    const visuel = event.lieu.visuel_principal != "" ? event.lieu.visuel_principal == "//www.mlg-consulting.com/manager_cc/events/lieux/img_uploaded/210322203353_sticker-region-sud-18.png" ? visuelComplet = `image du sud` : visuelComplet = `<p>ça c'est le visuel avec le village francophone</p>` : 'il n y a pas d image';
+    const visuel = event.lieu.visuel_principal != "" ? event.lieu.visuel_principal == "//www.mlg-consulting.com/manager_cc/events/lieux/img_uploaded/210322203353_sticker-region-sud-18.png" ? visuel = `image du sud` : visuel = `<p>ça c'est le visuel avec le village francophone</p>` : 'il n y a pas d image';
 
     console.log(visuel);
-    console.log('visuelcomplet : ' + visuelComplet);
+    // console.log('visuelcomplet : ' + visuelComplet);
 
     const content = `
 						    <div class="card mb-3" style="max-width:100%">
