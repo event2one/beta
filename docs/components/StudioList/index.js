b8fe4dcb4c529d5c1cf1c06b221961fb6f8f6a78
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-06-11 15:52:53 +0200

    correction style

--- docs/components/StudioList/index.js
+++ docs/components/StudioList/index.js
@@ -6,9 +6,9 @@ class StudioList extends HTMLElement {
     <div class="container mt-2">
     <div class="d-flex text-white" style="background-color: black;">
         <img class='m-2' src='https://picsum.photos/id/1041/150' />
-        <p style="text-align: center">
+        <p class='mt-3' style="text-align: center">
            <b>Le quartier général de la présence francophone sur l'évènement</b>
-           <br>Participez aux temps forts de la journée, rassemblez vous aux points de départ des parcours de visité et 
+           <br>Participez aux temps forts de la journée, rassemblez vous aux points de départ des parcours de visité et <br>
            rencontrez les décideurs francophones de l'évènements lors des soirées et sessions de pitch
         </p>
     </div>
@@ -16,7 +16,7 @@ class StudioList extends HTMLElement {
     <div class='d-flex flex-row justify-content-center align-items-center m-2'>
         <div class="card m-2" style="width: 28rem;">
             <img src="https://picsum.photos/id/1029/200" class="card-img-top" alt="...">
-            <div class="card-footer p-0" style="width: 100%; height: 100%; background-color: black; color: white;">
+            <div class="card-footer p-0" style="width: 100%; background-color: black; color: white;">
                 <p class="card-text fs-1 fw-bold p-2">Paris</p>
             </div>
         </div>
@@ -24,19 +24,19 @@ class StudioList extends HTMLElement {
 
         <div class="d-flex flex-column">
 
-            <div class="d-flex flex-row">
+            <div class="d-flex flex-row justify-content-center">
                 <div class="card m-2" style="width: 12rem;">
                     <img src="https://picsum.photos/id/1043/200" class="card-img-top" alt="...">
-                    <div class="card-footer" style="width: 100%; height: 100%; background-color: black; color: white;">
+                    <div class="card-footer" style="width: 100%; background-color: black; color: white;">
                         <p class="card-text">Tour Zamasky</p>
                     </div>
                 </div>
                 <p>Le quartier général : Lieu du studio central, quelques mots sur le lieu, adresse</p>
             </div>
-            <div class="d-flex flex-row">
+            <div class="d-flex flex-row justify-content-center">
                 <div class="card m-2" style="width: 12rem;">
                     <img src="https://picsum.photos/id/1044/200" class="card-img-top" alt="...">
-                    <div class="card-footer" style="width: 100%; height: 100%; background-color: black; color: white;">
+                    <div class="card-footer" style="width: 100%;  background-color: black; color: white;">
                         <p class="card-text">Salon vivatech</p>
                     </div>
                 </div>
