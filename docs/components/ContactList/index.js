2c6c04d539143f2f66c041cc2fe0086723209b70
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-06-25 10:02:06 +0200

    essai de splide

--- docs/components/ContactList/index.js
+++ docs/components/ContactList/index.js
@@ -1,16 +1,36 @@
 class ContactList extends HTMLElement {
-   
-    constructor() {
-        
-        super();
-   
-        this.innerHTML = `
-        
-        <section>
-            test
-        </section>`;
+  constructor() {
+    super();
 
-    }
+    this.innerHTML = `
+        <script>
+            var splide = new Splide( '#splide' );
+
+            splide.on( 'autoplay:playing', function ( rate ) {
+                console.log( rate ); // 0-1
+            } );
+            
+            splide.mount();
+        </script>
+        <section>
+            <div class="container">
+                <div class="splide">
+                    <div class="splide__track">
+                        <ul class="splide__list">
+                            <li class="splide__slide">Slide 01</li>
+                            <li class="splide__slide">Slide 02</li>
+                            <li class="splide__slide">Slide 03</li>
+                        </ul>
+                    </div>
+                
+                    <div class="splide__progress">
+                        <div class="splide__progress__bar">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>`;
+  }
 }
 
-customElements.define('contact-list', ContactList);
+customElements.define("contact-list", ContactList);
