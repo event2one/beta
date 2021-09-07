a541f2b70efd2701501854b119343c711797b653
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-09-07 14:15:21 +0200

    changement video

--- docs/components/VideoHeader/index.js
+++ docs/components/VideoHeader/index.js
@@ -3,13 +3,14 @@ class VideoHeader extends HTMLElement {
     super();
 
     this.innerHTML = `
-        <section>
-            <video controls width="250">
+        <section style="width: 100vw; height: 40vh;">
+            <video loop muted autoplay>
 
                 <source src="//www.mlg-consulting.com/manager_cc/docs/archives/french_village_video_cover_2018_1.mp4"
                     type="video/mp4">
                 Sorry, your browser doesn't support embedded videos.
             </video>
+            <h1 style="font-size: 4rem; font-weight: 800;">Bienvenue au point de convergence des solutions, des territoires et des décideurs francophones de l’innovation</h1>
         </section>`;
   }
 }
