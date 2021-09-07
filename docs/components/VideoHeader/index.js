a2d82f62a5e6a92e65ac96daf1e6f599055c7ef7
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-09-07 16:10:55 +0200

    test media

--- docs/components/VideoHeader/index.js
+++ docs/components/VideoHeader/index.js
@@ -4,12 +4,12 @@ class VideoHeader extends HTMLElement {
 
     this.innerHTML = `
     <style>
-    @media all and (max-device-width: 480px) {
-        h1 #video-header-title {
-            font-size= 6vw;
-            color: blue !important;
+        @media all and (max-device-width: 480px) {
+            h1 #video-header-title {
+                font-size: 6vw;
+                background-color: blue;
+            }
         }
-    }
     </style>
         <section style="height: 70vh; text-align: center">
             <video loop="" muted="" autoplay="" style="width: 100%;height: 70vh;object-fit: cover;">
@@ -18,7 +18,7 @@ class VideoHeader extends HTMLElement {
                     type="video/mp4">
                 Sorry, your browser doesn't support embedded videos.
             </video>
-            <h1 id="video-header-title" style="font-size: 6vh; margin: -50vh auto 0 auto; width: 85vw;">Bienvenue au point de convergence des solutions, des territoires et des décideurs francophones de l’innovation</h1>
+            <h1 id="video-header-title" style="font-size: 6vh; margin: -50vh auto 0 auto; width: 85vw; color: white !important;">Bienvenue au point de convergence des solutions, des territoires et des décideurs francophones de l’innovation</h1>
         </section>`;
   }
 }
