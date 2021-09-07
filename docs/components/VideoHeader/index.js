051ce7954df1815bc8b855424fbd1647bb05bab7
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-09-07 14:41:19 +0200

    changement css

--- docs/components/VideoHeader/index.js
+++ docs/components/VideoHeader/index.js
@@ -3,14 +3,14 @@ class VideoHeader extends HTMLElement {
     super();
 
     this.innerHTML = `
-        <section style="width: 100vw; height: 40vh;">
-            <video loop muted autoplay style="object-fit: cover; width: 100%; height: 100%;">
+        <section style="height: 70vh;">
+            <video loop="" muted="" autoplay="" style="width: 100%;height: 100vh;object-fit: cover;">
 
                 <source src="//www.mlg-consulting.com/manager_cc/docs/archives/french_village_video_cover_2018_1.mp4"
                     type="video/mp4">
                 Sorry, your browser doesn't support embedded videos.
             </video>
-            <h1 style="font-size: 3rem; font-weight: 800; color: white;">Bienvenue au point de convergence des solutions, des territoires et des décideurs francophones de l’innovation</h1>
+            <h1 style="font-size: 3rem; color: white !important; position:absolute;">Bienvenue au point de convergence des solutions, des territoires et des décideurs francophones de l’innovation</h1>
         </section>`;
   }
 }
