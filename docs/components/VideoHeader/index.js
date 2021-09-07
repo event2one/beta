9442c940671d43c4ca4d3461d154041ab44a02bf
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-09-07 14:10:20 +0200

    ajout vidéo

--- docs/components/VideoHeader/index.js
+++ docs/components/VideoHeader/index.js
@@ -1,13 +1,17 @@
 class VideoHeader extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.innerHTML = `
+    this.innerHTML = `
         <section>
-            <p>Video Header</p>
-        </section>`;
-    }
-}
+            <video controls width="250">
 
+                <source src="//www.mlg-consulting.com/manager_cc/docs/archives/french_village_video_cover_2018_1.mp4"
+                    type="video/mp4">
+                Sorry, your browser doesn't support embedded videos.
+            </video>
+        </section>`;
+  }
+}
 
 customElements.define("video-header", VideoHeader);
