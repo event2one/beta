0975dfbd9392dc32ee6e9b43affdffc6b4c4682c
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-12-23 15:50:05 +0100

    clg again

--- docs/components/ContactListFix/index.js
+++ docs/components/ContactListFix/index.js
@@ -36,7 +36,7 @@ class ContactListFix extends HTMLElement {
 
     
     this.currentStatutList = this.statutList.filter((statut) =>
-      this.statut.includes(statut)
+      console.log(this.statut.includes(statut))
     );
 
     console.log(this.currentStatutList)
