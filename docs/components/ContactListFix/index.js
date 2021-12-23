302c11e012119daccf9aad9a75ac6855a0ba1693
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2021-12-23 15:18:55 +0100

    changement grid système

--- docs/components/ContactListFix/index.js
+++ docs/components/ContactListFix/index.js
@@ -2,13 +2,13 @@ class ContactListFix extends HTMLElement {
   constructor() {
     super();
     //Titre
-    this.title = this.getAttribute("title");
+    this.title = this.getAttribute("title")
 
     //Gère le mode d'affichage
     this.displayMode = this.getAttribute("displayMode");
-    this.horizontalStyle = "";
-    if (this.displayMode === "horizontal") {
-      this.horizontalStyle = `style="display: flex; flex: 1 1 auto; height: 20vh; overflow: hidden;"`;
+    this.horizontalStyle = ""
+    if(this.displayMode === "horizontal") {
+      this.horizontalStyle = `style="display: flex; flex: 1 1 auto; height: 20vh; overflow: hidden;"`
     }
 
     this.showFlag = this.getAttribute("showFlag");
@@ -68,16 +68,8 @@ class ContactListFix extends HTMLElement {
                         </style>
 
                         <section class="container">
-                            <h2 class="text-center">${
-                              this.title ? this.title : ""
-                            }</h2>
-                                        <div class="ContactListFix row row-cols-1 row-cols-md-${
-                                          this.displayNumber
-                                            ? this.displayNumberList[
-                                                parseInt(this.displayNumber)
-                                              ]
-                                            : "3"
-                                        }">
+                            <h2 class="text-center">${this.title.length > 0 ? this.title : ""}</h2>
+                                        <div class="ContactListFix row">
                                         </div>
                         </section>`;
 
@@ -86,7 +78,7 @@ class ContactListFix extends HTMLElement {
 
   displayInfoContacts = ({ infoContact }) => {
     const content = `
-    <div class="col">
+    <div class="col-md-${this.displayNumber ? this.displayNumberList[parseInt(this.displayNumber)] : "3"}">
     <div class="card shadow">
                     <div ${this.horizontalStyle}>
                     <img class="card-img-top" src="${
